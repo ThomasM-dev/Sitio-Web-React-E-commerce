a3d5ed8cb62f2be5c2535754088d46dc230b24de
Thomasdevm <Thomasmadariaga2021@outlook.es>
2025-02-13 21:40:06 -0300

    esatilos

--- src/components/SlidesImg/index.jsx
+++ src/components/SlidesImg/index.jsx
@@ -32,15 +32,15 @@ const SliderImg = () => {
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 2,
+                    slidesToShow: 3,
                     slidesToScroll: 1,
                 },
             },
             {
                 breakpoint: 600,
                 settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
+                    slidesToShow: 3,
+                    slidesToScroll: 2,
                 },
             },
         ],
